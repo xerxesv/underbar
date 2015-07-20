6a7aac5d0a4f7e7d0004ad387e09f1f1b9832da0
xerxesv <xerxesv@gmail.com>
2015-07-20 13:53:47 -0700

    Added functions up to map

--- src/underbar.js
+++ src/underbar.js
@@ -106,6 +106,11 @@
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
+	  var mapped = [];
+	  _.each(collection,function(item,index,array){
+		 mapped.push(iterator(item,index,array));
+	  });
+	  return mapped;
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
